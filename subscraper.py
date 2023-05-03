d5e4ba3e2e58cd0feb21229f96d17b7ef2036477
EmirhanNadirKaraman <nadirkaraman2001@gmail.com>
2023-05-03 14:59:44 +0300

    Added video parsing logic

--- subscraper.py
+++ subscraper.py
@@ -1,9 +1,13 @@
+import json
 import os
-import youtube_transcript_api
 
 # !pip install google-api-python-client
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptFound, TranscriptsDisabled
+
+import scrapetube
 
 def youtube_authenticate():
     api_service_name = "youtube"
@@ -18,6 +22,24 @@ def youtube_authenticate():
 def main():
     print("main called")
     youtube = youtube_authenticate()
+
+    channels = json.loads(open('channels.json').read())
+
+    for channel in channels:
+        videos = scrapetube.get_channel(channel['id'])
+
+        for video_id in videos:
+            response = youtube.videos().list(
+                part="snippet",
+                id=video_id
+            ).execute()
+
+            print(response)
+            break
+
+
+
+
 
 
 if __name__ == '__main__':
